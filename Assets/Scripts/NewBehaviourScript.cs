41dca6d3dc708b159e68f1d722f69be2bc15ce3f
Hüseyin Paşa DEMİR <44347171+hsynpsdmr@users.noreply.github.com>
2020-08-08 23:31:44 +0300

    Initial commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float m_Speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axis_h = Input.GetAxis("Horizontal");
+
+		if (axis_h != 0)
+		{
+			Vector3 pos =transform.position;
+			pos.x += axis_h * m_Speed * Time.deltaTime;
+			transform.position= pos;
+		}   
+    }
+}
